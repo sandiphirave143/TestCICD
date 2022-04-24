9511322fdd7af7aecde626e51aa6ca4dab031bd7
Sandip Hirave <sandip.hirave@ardentisys.com>
2022-04-24 15:45:14 +0530

    first changes

---
+++ devops-learning-CICD/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace devops_learning_CICD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetName()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ unittest/Test/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using devops_learning_CICD.Controllers;
+
+namespace unittest
+{
+    public class TestClass
+    {
+        //[Fact]
+        //void Get_Success()
+        //{
+
+        //    ValuesController valuesController = new ValuesController();
+
+        //    var result = valuesController.GetName();
+        //    Assert.Equal("Hello World", result.Value);
+        //}
+    }
+}
